770d3efcf415023a1102a09379951b99b00f0a1f
Новиков Николай Андреевич <novikov.na@zaprib.ru>
2021-09-01 11:39:36 +0400

    Успешный тест CMake. Написал тестовое приложение "application.cpp". Написал "CMakeList.txt" файл. Написал скрипт "build.sh" для запуска процесса сборки.

---
+++ application.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Ну здарова, отец..." << endl;
+
+  return 0;
+}
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "\nBuilding project script...\n"
+
+cmake .
+make
+
+echo -e "\n...finished!\n"
