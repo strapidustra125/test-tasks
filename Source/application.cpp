cd1a1f5cc5c8ccc4c0c04eaed9fc104de2b49640
Новиков Николай Андреевич <strapidustra125@gmail.com>
2021-09-08 17:20:53 +0400

    Успешный запуск клинт-сервера в одном приложении.

--- Source/application.cpp
+++ Source/application.cpp
@@ -39,45 +39,38 @@ string ZMQStringFormat(int ID, int cmd)
 }
 
 
-std::string hexDump( const zmq::message_t & aMessage ) {
-  // I'm going to build a hex/ascii dump like you've seen so many times before
-  std::string  msg;
-  std::string  ascii;
-  // get the pointer to the start of the message's payload - and it's size
-  const char *buff = (const char *)aMessage.data();
-  int         size = ((zmq::message_t &)aMessage).size();
-  const char *end  = buff + size - 1;
-  // see if it's the trivial case
-  if (buff == NULL) {
-    msg.append("NULL");
-  } else {
-    // get a place to hold the conversion of each byte
-    char   hex[3];
-    bzero(hex, 3);
-    // run through the valid data in the buffer
-    for (const char *p = buff; p <= end; ++p) {
-      // generate the hex code for the byte and add it
-      snprintf(hex, 3, "%02x", (uint8_t)(*p));
-      msg.append(hex).append(" ");
-      // if it's printable, add it to the ascii part, otherwise, put a '.'
-      if (isprint(*p)) {
-        ascii.append(p, 1);
-      } else {
-        ascii.append(".");
-      }
-      // see if we have a complete line
-      if (ascii.size() >= 19) {
-        msg.append(" ").append(ascii).append("\n");
-        ascii.clear();
-      }
-    }
-    // if we have anything left, put it on the line as well
-    if (ascii.size() > 0) {
-      msg.append((19 - ascii.length())*3 + 1, ' ').append(ascii);
-    }
-  }
+string hexDump(const zmq::message_t & aMessage)
+{
+    // I'm going to build a hex/ascii dump like you've seen so many times before
+    string  msg;
+    string  ascii;
 
-  return msg;
+    const char *buff = (const char *)aMessage.data();       // Указатели на начало сообщения
+    int         size = ((zmq::message_t &)aMessage).size(); // Размер сообщения
+    const char *end  = buff + size - 1;                     // Указатель на конец сообщения
+
+    if (buff == NULL) msg.append("NULL");
+    else
+    {
+        // run through the valid data in the buffer
+        for (const char *p = buff; p <= end; ++p)
+        {
+            if (isprint(*p)) ascii.append(p, 1);
+            else ascii.append(".");
+
+            // see if we have a complete line
+            if (ascii.size() >= 19)
+            {
+                msg.append(ascii).append("\n");
+                ascii.clear();
+            }
+        }
+
+        // if we have anything left, put it on the line as well
+        if (ascii.size() > 0) msg.append(ascii);
+    }
+
+    return msg;
 }
 
 
@@ -142,9 +135,6 @@ int main(int argc, char *argv[])
     }
 
 
-    char* value = (char*)malloc(6);
-
-
     /* ======= Выбор фрагмента кода: клиент или сервер ======= */
 
     switch(serverFlag)
@@ -159,21 +149,13 @@ int main(int argc, char *argv[])
 
         currentApplicationID = genNewID();
 
-        cout << "Application ID: " << currentApplicationID << endl;
+        cout << "Application ID: " << currentApplicationID << endl << endl;
 
         while(1)
         {
-            requestMessage.rebuild();
-            cout << "Message:    " << requestMessage.data() << endl;
-
-
-
-            //zmqReplaySocket.recv(&replayMessageString, 6);
             zmqReplaySocket.recv(&replayMessage);
 
             cout << "Server got: " << hexDump(replayMessage) << endl;
-
-            //sleep(1);
 
             replayMessage.rebuild("123456", 6);
             zmqReplaySocket.send(replayMessage);
@@ -193,22 +175,19 @@ int main(int argc, char *argv[])
 
     case false:
 
-
-
         cout << "It's a client!" << endl << endl;
         cout << "Connection status: " << zmqReplaySocket.connected() << endl;
         replayMessageString = "";
 
         zmqRequestSocket.connect(socketAddress);
-        memcpy (requestMessage.data (), ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest).c_str(), ZMQ_MESSAGE_SIZE);
-        //requestMessage.rebuild(ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest).c_str(), ZMQ_MESSAGE_SIZE);
+        requestMessage.rebuild(ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest).c_str(), ZMQ_MESSAGE_SIZE);
         zmqRequestSocket.send(requestMessage);
 
         cout << "Client send: " << ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest) << endl;
 
-        zmqRequestSocket.recv(&replayMessageString, ZMQ_MESSAGE_SIZE);
+        zmqRequestSocket.recv(&replayMessage);
 
-        cout << "Client got: " << replayMessageString << endl;
+        cout << "Client got: " << hexDump(replayMessage) << endl;
 
 
         break;
@@ -221,194 +200,8 @@ int main(int argc, char *argv[])
 
     while(1)
     {
-        try
-        {
-            zmqRequestSocket.recv(&replayMessageString, ZMQ_MESSAGE_SIZE);
-        }
-        catch(zmq::error_t err)
-        {
 
-        }
-
-        cout << "Client got: " << replayMessageString << endl;
-
-        sleep(1);
     }
-
-
-
-
-    //serverFlag = zmq_bind(zmqRepSocket, socketAddress.c_str());
-
-    // Создание клиентского сокета
-    //void * zmqReqSocket = zmq_socket(zmqContext, ZMQ_REQ);
-
-    // context_t zmqContext;
-    // socket_t zmqRepSocket(zmqContext, socket_type::push);
-    // message_t msg;
-
-    // zmqRepSocket.bind(socketAddress);
-
-    // msg.rebuild(IDRequestString.c_str(), IDRequestString.length());
-
-    // zmqRepSocket.send(msg);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // if(serverFlag == 0)
-    // {
-    //     // Значит это - первое запущенное приложение
-
-    //     cout << "I'm a server!" << endl;
-
-    //     currentApplicationID = genNewID();
-
-    //     cout << "Application ID: " << currentApplicationID << endl;
-    // }
-    // else
-    // {
-    //     // Значит сервер уже проинициализировал сокет.
-
-    //     cout << "I'm a client!" << endl;
-
-    //     // Подключение к открытому сервером сокету
-    //     zmq_connect(zmqReqSocket, socketAddress.c_str());
-
-
-    //     zmq_msg_init_size(&sendMessage, IDRequestString.length());
-    //     memcpy(zmq_msg_data(&sendMessage), IDRequestString.c_str(), IDRequestString.length());
-    //     rc = zmq_msg_send(&sendMessage, zmqReqSocket, 0);
-
-    //     if(rc == -1) cout << "ID request error! rc = " << rc << "." << endl;
-
-    //     zmq_msg_close(&sendMessage);
-
-
-
-    //     //currentApplicationID = genNewID();
-
-    //     //cout << "Application ID: " << currentApplicationID << endl;
-    // }
-
-    // //char * value;
-
-    // cout << "waiting..." << endl;
-
-    // int i = 0;
-
-    // while(i++ < 10)
-    // {
-    //     //i++;
-    //     zmq_msg_t receiveMessage;
-    //     zmq_msg_init(&receiveMessage);
-
-    //     rc = zmq_msg_recv(&receiveMessage, zmqRepSocket, 0);
-
-    //     if(rc == -1)
-    //     {
-    //         //cout << "New message error! rc = " << rc << "." << endl;
-    //         cout << rc << endl;
-    //     }
-    //     else
-    //     {
-    //         int length = zmq_msg_size(&receiveMessage);
-    //         char* value = (char*)malloc(length);
-    //         memcpy(value, zmq_msg_data(&receiveMessage), length);
-    //         zmq_msg_close(&receiveMessage);
-    //         printf("%s\n", value);
-    //         free(value);
-
-    //         // value = (char *)malloc(zmq_msg_size(&receiveMessage));
-    //         // memcpy(value, zmq_msg_data(&receiveMessage), zmq_msg_size(&receiveMessage));
-
-    //         // cout << "New message: " << value << endl;
-
-
-    //     }
-
-    //     // value = (char *)malloc(zmq_msg_size(&receiveMessage));
-    //     // memcpy(value, zmq_msg_data(&receiveMessage), zmq_msg_size(&receiveMessage));
-
-    //     // cout << "New message: " << value << endl;
-
-    //     //zmq_msg_close(&receiveMessage);
-    //     rc = 0;
-
-
-
-
-
-
-
-
-
-
-    //     sleep(1);
-    // }
-
-
-
-
-
-
-
-
-
-
-    // cout << "Destroy context!" << endl;
-
-    // zmq_close(zmqRepSocket);
-    // zmq_close(zmqReqSocket);
-
-    // zmq_ctx_destroy(zmqContext);
-
-    // cout << "End of program!" << endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
