a1314583b7e7e0d8884d9955dc639fc19ffd67d9
Новиков Николай Андреевич <strapidustra125@gmail.com>
2021-09-09 13:42:37 +0400

    Рабочая версия инициализации клиента. Клиент успешно получает от сервера случайный ID. Добавил класс zmq_cmd для хранения команд.

---
+++ Includes/zmq_cmd.hpp
@@ -0,0 +1,92 @@
+/********************************************************************************
+ *
+ * Класс для хранения команды, передаваемой в сообщении ZeroMQ
+ *
+ *******************************************************************************/
+
+#ifndef _ZMQ_CMD_HPP_
+#define _ZMQ_CMD_HPP_
+
+#include <string>
+
+using namespace std;
+
+class zmq_cmd
+{
+public:
+    int commandID;
+    int applicationID;
+    int commandData;
+
+    zmq_cmd(int cmdID=0, int appID=0, int cmdData=0)
+    {
+        commandID = cmdID;
+        applicationID = appID;
+        commandData = cmdData;
+    }
+
+    string formatString();
+    void readString(string inputString);
+
+};
+
+string zmq_cmd::formatString()
+{
+    return  ((commandID == 0) ? "00" : to_string(commandID)) +
+            ((applicationID == 0) ? "0000" : to_string(applicationID)) +
+            ((commandData == 0) ? "0000" : to_string(commandData));
+}
+
+void zmq_cmd::readString(string inputString)
+{
+    string tempStr = "";
+    tempStr += inputString[0];
+    tempStr += inputString[1];
+    commandID = stoi(tempStr);
+
+    tempStr = "";
+    tempStr += inputString[2];
+    tempStr += inputString[3];
+    tempStr += inputString[4];
+    tempStr += inputString[5];
+    applicationID = stoi(tempStr);
+
+    tempStr = "";
+    tempStr += inputString[6];
+    tempStr += inputString[7];
+    tempStr += inputString[8];
+    tempStr += inputString[9];
+    commandData = stoi(tempStr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- Source/application.cpp
+++ Source/application.cpp
@@ -7,8 +7,10 @@
 #include <unistd.h>	// Для функции sleep()
 #include <cstdlib> 	// Для функций rand() и srand()
 #include <ctime> 	// Для функции time()
+#include <algorithm>
 
 #include "zmq.hpp"
+#include "zmq_cmd.hpp"
 
 using namespace std;
 using namespace zmq;
@@ -16,30 +18,69 @@ using namespace zmq;
 
 /* ================================ Константы ================================ */
 
-#define ZMQ_MESSAGE_SIZE        6
+#define ZMQ_MESSAGE_SIZE        10
 
-// Команды ZeroMQ
+// Команды Клиента
 #define ZMQ_CMD_IDRequest       10
 #define ZMQ_CMD_KillAll         11
+
+// Команды сервера
+#define ZMQ_CMD_ID              20
+
 
 
 /* ========================= Вспомогательные функции ========================= */
 
-// Строка случайных четырехзначных чисел
-int genNewID()
+// Новый случайный ID
+int genNewID(vector<int> vecID)
 {
-    return 1000 + rand() % 9000;
+    int newID;
+    while(1)
+    {
+        newID = 1000 + rand() % 9000;
+
+        if(find(vecID.begin(), vecID.end(), newID) == vecID.end())
+        {
+            return newID;
+        }
+    }
+
 }
 
 
 // Формирование строки для ZMQ сообщения
-string ZMQStringFormat(int ID, int cmd)
+string ZMQStringFormat(int cmd, int ID)
 {
-    return (ID == 0) ? "0000" + to_string(cmd): to_string(ID) + to_string(cmd);
+    return (ID == 0) ? to_string(cmd) + "0000": to_string(cmd) + to_string(ID);
 }
 
 
-string hexDump(const zmq::message_t & aMessage)
+// Получение команды из сообщения
+int getCmdFromString(string ZMQMessageString)
+{
+    string cmd = "";
+    cmd += ZMQMessageString[0];
+    cmd += ZMQMessageString[1];
+
+    return stoi(cmd);
+}
+
+
+// Получение ID из сообщения
+int getIDFromString(string ZMQMessageString)
+{
+    string cmd = "";
+    cmd += ZMQMessageString[2];
+    cmd += ZMQMessageString[3];
+    cmd += ZMQMessageString[4];
+    cmd += ZMQMessageString[5];
+
+    return stoi(cmd);
+}
+
+
+// Получение передаваемой информации из ZMQ сообщения
+string getStringFromZMQMessage(const zmq::message_t & aMessage)
 {
     // I'm going to build a hex/ascii dump like you've seen so many times before
     string  msg;
@@ -74,6 +115,11 @@ string hexDump(const zmq::message_t & aMessage)
 }
 
 
+
+
+
+
+
 /* ============================= Основная функция ============================ */
 
 
@@ -83,23 +129,36 @@ int main(int argc, char *argv[])
 
     /* ======= Общие переменные ======= */
 
-    context_t zmqContext;                           // Создание контекста
-    socket_t zmqReplaySocket(zmqContext, socket_type::rep);
+    // Создание контекста
+    context_t zmqContext;
+
+    // Сокеты для запроса и ответа
     socket_t zmqRequestSocket(zmqContext, socket_type::req);
+    socket_t zmqReplaySocket(zmqContext, socket_type::rep);
 
-    string socketAddress = "tcp://127.0.0.1:4444";  // Сокет для подключения
-    bool serverFlag = true;					        // Флаг первой запущенной программы
-    int currentApplicationID = 0;                   // ID текущего экземпляра приложения
+    // Сообщение для отправки по ZeroMQ
+    message_t zmqMessage(ZMQ_MESSAGE_SIZE);
 
-    message_t   requestMessage(ZMQ_MESSAGE_SIZE),                     // ZMQ сообщение для запроса
-                replayMessage(ZMQ_MESSAGE_SIZE);                      // ZMQ сообщение для ответа
+    // Команда для обмена между клиентами и сервером
+    zmq_cmd zmqCommand(0, 0, 0);
 
-    string replayMessageString = "";                //
+    // Адрес сокета для подключения
+    string socketAddress = "tcp://127.0.0.1:4444";
+
+    // Флаг первой запущенной программы
+    int serverFlag = 1;
+
+    // ID текущего экземпляра приложения
+    int currentApplicationID = 0;
 
 
     /* ======= Серверные переменные ======= */
 
     vector <int> IDvector;
+    int newID = 0;
+
+    string messageString = "";                //
+
 
 
     /* ======= Клиентские переменные ======= */
@@ -108,6 +167,8 @@ int main(int argc, char *argv[])
 
 
     srand(time(0)); // Стартовое число ГПСЧ
+
+
 
 
     /* ======= Конфигурация обмена сообщениями ======= */
@@ -123,7 +184,7 @@ int main(int argc, char *argv[])
         // Если не получилось, значит сервер его уже открыл
 
         cout << "Extension..." << endl;
-        serverFlag = false;
+        serverFlag = 0;
 
         if(zmqReplaySocket.connected())
         {
@@ -133,6 +194,11 @@ int main(int argc, char *argv[])
             cout << "Close socket..." << endl;
         }
     }
+
+
+
+
+
 
 
     /* ======= Выбор фрагмента кода: клиент или сервер ======= */
@@ -147,21 +213,58 @@ int main(int argc, char *argv[])
         cout << "It's a server!" << endl << endl;
         cout << "Connection status: " << zmqReplaySocket.connected() << endl;
 
-        currentApplicationID = genNewID();
+        currentApplicationID = genNewID(IDvector);
+        IDvector.push_back(currentApplicationID);
 
         cout << "Application ID: " << currentApplicationID << endl << endl;
 
+
+
         while(1)
         {
-            zmqReplaySocket.recv(&replayMessage);
+            zmqReplaySocket.recv(&zmqMessage);
+            messageString = getStringFromZMQMessage(zmqMessage);
 
-            cout << "Server got: " << hexDump(replayMessage) << endl;
+            cout << "Server got: " << messageString << endl;
 
-            replayMessage.rebuild("123456", 6);
-            zmqReplaySocket.send(replayMessage);
+            switch (getCmdFromString(messageString))
+            {
 
-            cout << "Server send: 123456" << endl;
+            /* ======= Запрос ID ======= */
+
+            case ZMQ_CMD_IDRequest:
+
+                cout << "Got command \"ZMQ_CMD_IDRequest\": " << ZMQ_CMD_IDRequest << endl;
+
+                newID = genNewID(IDvector);
+                messageString = ZMQStringFormat(ZMQ_CMD_ID, newID);
+                IDvector.push_back(newID);
+
+                break;
+
+
+            case ZMQ_CMD_KillAll:
+
+                break;
+
+            default:
+                break;
+            }
+
+
+
+
+            zmqMessage.rebuild(messageString.c_str(), ZMQ_MESSAGE_SIZE);
+            zmqReplaySocket.send(zmqMessage);
+
+            cout << "Server send: " << messageString << endl;
         }
+
+        sleep(5);
+
+        zmqMessage.rebuild("123456", ZMQ_MESSAGE_SIZE);
+        zmqReplaySocket.send(zmqMessage);
+
 
 
         break;
@@ -177,17 +280,21 @@ int main(int argc, char *argv[])
 
         cout << "It's a client!" << endl << endl;
         cout << "Connection status: " << zmqReplaySocket.connected() << endl;
-        replayMessageString = "";
 
         zmqRequestSocket.connect(socketAddress);
-        requestMessage.rebuild(ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest).c_str(), ZMQ_MESSAGE_SIZE);
-        zmqRequestSocket.send(requestMessage);
+        zmqMessage.rebuild(ZMQStringFormat(ZMQ_CMD_IDRequest, currentApplicationID).c_str(), ZMQ_MESSAGE_SIZE);
+        zmqRequestSocket.send(zmqMessage);
 
-        cout << "Client send: " << ZMQStringFormat(currentApplicationID, ZMQ_CMD_IDRequest) << endl;
+        cout << "Client send: " << ZMQStringFormat(ZMQ_CMD_IDRequest, currentApplicationID) << endl;
 
-        zmqRequestSocket.recv(&replayMessage);
+        zmqRequestSocket.recv(&zmqMessage);
+        messageString = getStringFromZMQMessage(zmqMessage);
 
-        cout << "Client got: " << hexDump(replayMessage) << endl;
+        cout << "Client got: " << messageString << endl;
+
+        currentApplicationID = getIDFromString(messageString);
+
+        cout << "Application ID: " << currentApplicationID << endl << endl;
 
 
         break;
